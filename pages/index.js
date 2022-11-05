0823e2f835b5746129172501b123563f55c06aa9
Julian <julian_delisio@hotmail.com>
2022-11-04 23:06:01 -0300

    rutas dinamicas

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div >
       <Navbar/>
-      <h1>Practicas con NextJs</h1>
+      <h1>Practicas con NextJS</h1>
       <div>
       <Link href="/users">
         <button type="button"><a>USER</a></button>
